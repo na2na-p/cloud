b88af766e80f393529f841a238821255d983c425
na2na <49822810+na2na-p@users.noreply.github.com>
2022-07-30 12:48:14 +0900

    Update

--- ubuntu20.4/init.sh
+++ ubuntu20.4/init.sh
@@ -4,7 +4,7 @@ apt-get update -qq
 apt-get upgrade -qq -y
 
 # Install packages
-apt-get install -qy nginx expect
+apt-get install -qq -y nginx expect
 
 # expect Setting
 timeout=10
@@ -23,7 +23,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/
 
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list | /dev/null
 
 apt-get update -qq
 apt-get install -qq -y docker-ce docker-ce-cli containerd.io
