88f6311d205966facab095a15e692e43720cce90
na2na <49822810+na2na-p@users.noreply.github.com>
2022-07-30 12:39:43 +0900

    Update

--- ubuntu20.4/init.sh
+++ ubuntu20.4/init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 apt-get update
-apt-get upgrade -y
+apt-get upgrade -qy
 
 # Install packages
-apt-get install -y nginx expect
+apt-get install -qy nginx expect
 
 # expect Setting
 timeout=10
 force="y"
 
 # Setting up Docker & Docker-Compose
-apt-get install -y \
+apt-get install -qy \
     ca-certificates \
     curl \
     gnupg \
@@ -26,7 +26,7 @@ echo \
 $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -qy docker-ce docker-ce-cli containerd.io
 usermod -aG docker ${USER}
 
 mkdir -p /usr/local/libexec/docker/cli-plugins
