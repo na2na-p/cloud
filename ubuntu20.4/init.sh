4f714e68822b8f02d56fe35e77ca5611ae7f680b
na2na <49822810+na2na-p@users.noreply.github.com>
2022-07-30 12:50:54 +0900

    Fix

--- ubuntu20.4/init.sh
+++ ubuntu20.4/init.sh
@@ -23,7 +23,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/
 
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list | /dev/null
+$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update -qq
 apt-get install -qq -y docker-ce docker-ce-cli containerd.io
