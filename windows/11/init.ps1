26724d2014a7a3030c72d661605fa38f4cd0ce6e
na2na-p <na2na@na2na.dev>
2022-10-12 17:22:58 +0900

    Update

--- windows/11/init.ps1
+++ windows/11/init.ps1
@@ -1,6 +1,5 @@
 winget install -e --id Git.Git
 winget install -e --id GitHub.cli
-winget install -e --id Discord.Discord
 winget install -e --id Microsoft.PowerToys
 winget install -e --id JetBrains.Toolbox
 winget install -e --id Amazon.AWSCLI
@@ -8,7 +7,10 @@ winget install -e --id Amazon.SessionManagerPlugin
 winget install -e --id Microsoft.AzureCLI
 winget install -e --id VivaldiTechnologies.Vivaldi
 winget install -e --id Google.Chrome
+winget install -e --id Mozilla.Firefox
+winget install -e --id Mozilla.Thunderbird
 winget install -e --id SlackTechnologies.Slack
+winget install -e --id Discord.Discord
 winget install -e --id Microsoft.PowerShell
 winget install -e --id Docker.DockerDesktop
 winget install -e --id CrystalDewWorld.CrystalDiskInfo.KureiKeiEdition
