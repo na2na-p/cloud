41804778d15a898f5b6e7163f984a918c059e984
na2na-p <na2na@na2na.dev>
2022-10-12 18:19:02 +0900

    Update

--- windows/11/home.ps1
+++ windows/11/home.ps1
@@ -1,7 +1,7 @@
 $script_dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-& $script_dir\drivers.ps1
-& $script_dir\init.ps1
+& $script_dir\parts\drivers.ps1
+& $script_dir\parts\init.ps1
 
 winget install -e --id OBSProject.OBSStudio
 winget install -e --id Valve.Steam
