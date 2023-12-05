4eec2320ce40f38e101e6d8bf956c21da2052698
na2na-p <na2na@na2na.dev>
2023-12-05 15:39:51 +0900

    MicroK8s+ArgoCD

--- microk8s/setup/argocd.sh
+++ microk8s/setup/argocd.sh
@@ -1,7 +1,10 @@
-microk8s enable dns rbac storage
+microk8s enable dns
+microk8s enable rbac
+microk8s enable hostpath-storage
 microk8s kubectl create namespace argocd
 microk8s kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 microk8s kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 microk8s enable calico
 microk8s kubectl apply -f microk8s/argocd-network-policy.yaml
 microk8s kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode; echo
+microk8s kubectl patch svc argocd-server -n argocd --type='json' -p '[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":32455}]'
